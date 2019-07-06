22ad0752d83e3f2b17cd34019b4fc7e1d519feed
atharvajava <atharva@mail.uni-paderborn.de>
2019-07-06 21:07:43 +0200

    created navbar and basic index template

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,37 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import intro from "../images/intro.png"
+import Typed from 'react-typed';
 
-const IndexPage = () => (
+
+const IndexPage = () => {
+ 
+  return (
   <Layout>
     <SEO title="Home" />
-    <h1>Home Page</h1>
+    <p className="is-size-5-mobile has-text-centered">Full Stack Web Developer
+    <p className="is-size-7-mobile has-text-centered index-title-description">Have a look at my portfolio and job history.</p></p>
+    <img alt="" src={intro}/>
+    <p className="is-size-6-mobile has-text-justified">Welcome to Atharva's blog. Get informed, collaborate and discover projects I have worked on through the years!</p><br/>  
+    <Typed
+      className="is-size-6-mobile has-text-danger"
+      strings={[
+          'Skills = <b>React</b>',
+          'Skills = <b>Python</b>',
+          'Skills = <b>Golang</b>',
+          'Skills = <b>Java</b>',
+          'Skills = <b>NodeJS</b>',
+          'Skills = <b>Angular</b>']}
+          backDelay={1000}
+          loop
+          typeSpeed={40}
+          backSpeed={50} 
+          showCursor
+          cursorChar="|" /><br/><br/>
+              
+    <p className="is-size-6-mobile has-text-justified">Have a look at my work or check out my blogs <b></b> happy exploring <b></b></p>
   </Layout>
 )
-
+}
 export default IndexPage
