00e6e616cb1ce6fe25feaf9e9e854e3a30c139c0
atharvajava <atharva@mail.uni-paderborn.de>
2019-07-07 19:52:08 +0200

    Added social media share and disqus

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -7,34 +7,50 @@ import Img from "gatsby-image"
 import {slugify} from "../util/utilityFunction"
 import ReactHtmlParser from "react-html-parser"
 import authors from "../util/authors"
+import { DiscussionEmbed } from "disqus-react"
 
-const SinglePost=({data})=>{
+const SinglePost=({data , pageContext})=>{
     const post = data.markdownRemark.frontmatter
     const author = authors.find(x=> x.name === post.author)
-    console.log(data)
+    const baseUrl= "https://atharvapandey.com/"
+
+    const disquesShortname = "atharvapandey"
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug
+    }
     return (
         <Layout>
             <SEO title={post.title}/>
-            <p className="is-size-3-mobile">{post.title}</p>
+            <p className="is-size-3">{post.title}</p>
             <div className="columns">
                 <div className="column is-two-thirds">
                     <div className="card">
                         <Img fluid={post.image.childImageSharp.fluid}/>
-                    </div>
-                    <div className="content single-blog-content">
+                    
+                    <div className="card-content single-blog-content">
                     <div>{ReactHtmlParser(data.markdownRemark.html)}</div>
                     <br/><br/>
-                    <time className="has-text-right" dateTime="2016-1-1">{post.date}</time>
+                    <time className="has-text-right time-tag" dateTime="2016-1-1">{post.date}
                     <br/>
                     {post.tags.map((tag) =>{
                         
                         return (
                         <Link key={tag} to={`/tag/${slugify(tag)}`}>
-                            <span className="tag is-black is-uppercase">{tag}</span>&nbsp;
+                            <span className="tag is-black is-uppercase time-tag">{tag}</span>
                         </Link>
                     )})}
-                    <Link className="media-right button" to="/blog">Back</Link>
+                    </time>
+                    <div className="social-share-chirps">
+                        <span className="facebook"><a href={"https://www.facebook.com/sharer/sharer.php?u="+ baseUrl + pageContext.slug} target="_blank" rel="noopener noreferrer"></a></span>
+                        <span className="twitter"><a href={"https://twitter.com/share?url="+ baseUrl + pageContext.slug+"&text="+post.title+"&via"+"twitterHandle"} target="_blank" rel="noopener noreferrer"></a></span>
+                        <span className="linked-in"><a href={"https://www.linkedin.com/shareArticle?url="+ baseUrl + pageContext.slug} target="_blank" rel="noopener noreferrer"></a></span>
+                        <Link className="button back" to="/blog">Back</Link>
+                    </div>
+                    </div>
                 </div>
+                <DiscussionEmbed shortname={disquesShortname} config={disqusConfig}/>
                 </div>
                 <div className="is-hidden-mobile column is-one-third blog-sidebar">
                     <Sidebar author={author} authorImageFluid={data.file.childImageSharp.fluid}/>
