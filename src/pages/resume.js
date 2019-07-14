d163ab80f97d8ad2f3c34ce09f1dfc7e3bcee900
atharvajava <atharvajava@gmail.com>
2019-07-14 17:44:22 +0200

    fixed iframe

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,11 +5,11 @@ import SEO from "../components/seo"
 
 const CVPage = () => (
   <Layout>
-    <SEO title="CV" />
+    <SEO title="Resume" />
     <p className="is-size-2 has-text-centered other-page-title">RESUME</p>
     <div className="resume-wrapper" >
      
-          <iframe title="Atharva's Resume" src={"resume.pdf"} type="application/pdf" frameBorder="0"  allowFullScreen/>
+          <iframe title="Atharva's Resume" src={"resume.pdf"} type="application/pdf" frameBorder="0" width="100%" height="100%" allowFullScreen/>
   
     </div>
   </Layout>
