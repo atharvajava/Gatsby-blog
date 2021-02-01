df03789261946cdbb9ae9e8debdff166af4c17f0
Atharva <atharvajava@gmail.com>
2021-02-01 22:41:49 +0100

    sidebar fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navigation from "./navigation"
 
 interface Props {
   location: Location
@@ -28,15 +29,18 @@ const Layout = ({ location, title, children }: Props) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <React.Fragment>
+      <Navigation />
+      <div className="global-wrapper" data-is-root-path={isRootPath}>
+        <header className="global-header">{header}</header>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </React.Fragment>
   )
 }
 
